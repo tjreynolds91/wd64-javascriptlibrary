81589ece11227f51f6f051a3bbbcbace95572b1d
tjreynolds91 <chinchilla@babies.com>
2020-09-19 19:30:00 -0400

    9/19

--- grammarTypes/01-comments.js
+++ grammarTypes/01-comments.js
@@ -16,3 +16,36 @@
 -Point 3 
 -ect.
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let favMovie = {
+        nameOfMovie: "Monty Python and The Holy Grail",
+        runTime:97,
+        characters:
+            [ {charOne: {name:'Tim?', age: 'unknown', items:["Hat with Horns", 'Spits while talks']}},
+              {charTwo: {name:'Black Knight', age: 42 , items:["Helmet", 'No limbs!']}}] ,
+        genre: "Comedy"
+
+    }
+
+
+console.log(favMovie.nameOfMovie);
+console.log(favMovie.runTime);
+console.log(favMovie.characters);
+console.log(favMovie.characters[1].charTwo.name);
+console.log(favMovie.characters[1].charTwo.items[1]);
