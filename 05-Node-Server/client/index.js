9c885a29cac990047a7a96b7a825e2f4c4d906b7
tjreynolds91 <tjreynolds91@gmail.com>
2020-10-15 22:18:11 -0400

    10/14

--- 05-Node-Server/client/index.js
+++ 05-Node-Server/client/index.js
@@ -57,3 +57,58 @@ function postToOneArrow(){
     .catch(error => console.error('Error:', error))
     .then(response => console.log('Success:', response));
 }
+
+function postData() {
+    let content = {testdata: { item: 'This was saved!'}};
+
+    let testDataAfterFetch = document.getElementById('test-data');
+    let createdAtAfterFetch = document.getElementById('created-at');
+
+    fetch('http://localhost:3000/test/seven', {
+       method: 'POST',
+       headers: {
+           'Content-Type': 'application/json'
+       },
+       body: JSON.stringify(content)
+    })
+    .then(response => response.json())
+    .then(function (text) {
+        console.log(text);
+
+        testDataAfterFetch.innerHTML = text.testdata.testdata;
+        createdAtAfterFetch.innerHTML = text.testdata.createdAt;
+    });
+}
+
+/******************
+* 4 GET FROM /ONE - Display Data
+******************/
+function fetchFromOneDisplayData(){
+
+    let url ='http://localhost:3000/test/one';
+    let dataView = document.getElementById('display-one');
+
+    fetch(url, {
+        method: 'GET',
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    }).then(
+        function(response){
+            return response.json()
+        })
+    .catch(
+        function(error){
+            console.error('Error:', error)
+        })
+    .then(
+        function(results){
+            let myList = document.querySelector('#getjson');
+            for (r of results){
+                console.log('Response:', r.testdata);
+                var listItem = document.createElement('li');
+                listItem.innerHTML = r.testdata;
+                myList.appendChild(listItem);
+            }
+        });
+}
